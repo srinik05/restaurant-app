4821b396f8eb866cdd7a8a39f221bc074dfc272b
srinik05 <srinivasu.jsb6@gmail.com>
2023-06-25 09:42:24 -0400

    removed unused classes

--- src/main/java/com/assignment/restaurant/app/repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.assignment.restaurant.app.repository;
-
-import com.assignment.restaurant.app.entity.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookingRepository extends JpaRepository<Reservation, Long> {
-}
